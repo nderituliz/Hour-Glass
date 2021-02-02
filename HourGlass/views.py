cdef31997b729312345c286faaa4254fb346afb7
Elizabeth Nderitu <nderituliz@gmail.com>
2021-02-02 23:53:18 +0300

    create the function home

--- HourGlass/views.py
+++ HourGlass/views.py
@@ -6,6 +6,27 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import CreateView, ListView,DetailView,UpdateView,DeleteView
 
-
-
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def home(request):
+    diary = Diary.objects.all()
+    context = {
+    "diary":diary,
+    }
+    return render(request, 'index.html', locals())
+
+def registration(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            form.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'registration/register.html', context)
+
+
