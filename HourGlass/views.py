872ac3a961d19bfaa22cb19ca483dbc76a8ce1e4
Elizabeth Nderitu <nderituliz@gmail.com>
2021-02-02 22:54:23 +0300

    initial commit

--- HourGlass/views.py
+++ HourGlass/views.py
@@ -1,12 +1,3 @@
-from django.shortcuts import render
-from django.shortcuts import render,redirect
-from django.contrib.auth.decorators import login_required
-from .forms import *
-from .models import *
-from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import CreateView, ListView,DetailView,UpdateView,DeleteView
-
 
 
 
