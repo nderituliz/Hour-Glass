70fae1ca926576018917629016bbabce6a09c5de
Elizabeth Nderitu <nderituliz@gmail.com>
2021-02-03 00:31:38 +0300

    create a class to post list

--- HourGlass/views.py
+++ HourGlass/views.py
@@ -29,6 +29,31 @@ def registration(request):
     }
     return render(request, 'registration/register.html', context)
 
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    diary = Diary.objects.all()
+    posts = Profile.objects.all()
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Your account has been successfully updated')
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+    'user_form':user_form,
+    'profile_form':profile_form,
+    'posts':posts,
+    'diary':diary,
+    }
+    return render(request, 'profile/profile.html', context)
+
 @login_required(login_url='/accounts/login/')
 def updateprofile(request):
     diary = Diary.objects.all()
@@ -69,21 +94,29 @@ def postsubject(request):
     context = {
         'form':form,
     }
-    return render(request, 'PostSubject.html', context)
+    return render(request, 'PostDiary.html', context)
 
 @login_required(login_url='/accounts/login/')
 def get_diary(request, id):
     diary = Diary.objects.get(pk=id)
-    return render(request, 'diary.html', {'diary':diary)
+    return render(request, 'diary.html', {'diary':diary})
 
 @login_required(login_url='/accounts/login/')
 def search_diary(request):
     if 'diary' in request.GET and request.GET['diary']:
         search_term = request.GET["diary"]
-        searched_diary = Diary.search_diary(search_term)
+        searched_diary = .search_diary(search_term)
         message = f"{search_term}"
 
         return render(request, 'search.html', {"message":message, "diary": searched_diary})
     else:
         message = "You haven't searched for any user"
         return render(request, 'search.html', {"message":message})
+
+
+# <app>/<model>_<viewtype>.html
+class PostListView(ListView):
+    model = Diary   
+    template_name = 'index.html'   
+    context_object_name = 'diary'
+    ordering = ['-pub_date']
